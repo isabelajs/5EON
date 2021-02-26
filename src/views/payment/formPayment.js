26b40e795b62cc4aa8d783b1940d805db2e6c75d
JuanC-JC <juancchaparro97@gmail.com>
2021-02-25 20:43:13 -0500

    change algorithm luhn

--- src/views/payment/formPayment.js
+++ src/views/payment/formPayment.js
@@ -8,7 +8,6 @@ const cFormPayment = () =>{
     window.document.body.appendChild(cFooterPayment())
 
     //on error colocar un label debajo indicado el error
-
     const cFormPayment = document.createElement('div')
     cFormPayment.classList.add('l-payment')
     cFormPayment.innerHTML = `<form id="paymentForm"  action="" class="c-form">
@@ -28,7 +27,7 @@ const cFormPayment = () =>{
                                                 </label>
                                                     
                                                 <label class="c-form-input" for="userEmail">
-                                                    Correo electronico<input type="email" id="userEmail" >
+                                                    Correo electronico<input type="text" id="userEmail" >
                                                 </label>
                                             </div>
                                             
@@ -90,7 +89,7 @@ const cFormPayment = () =>{
                                 
                                                 <label class="c-form-input c-form-input--icon" for="cardNumber">
                                                     Numero de tarjeta
-                                                    <i class="fas fa-credit-card"></i> 
+                                                    <i id="iconCard" class="fas fa-credit-card"></i> 
                                                     <input type="text" id="cardNumber" >
                                                 </label> 
                                 
@@ -136,78 +135,28 @@ const cFormPayment = () =>{
     const shippingHome = cFormPayment.querySelector('#shippingHome')
     const shippingStore = cFormPayment.querySelector('#shippingStore')
 
+    const cardNumber = cFormPayment.querySelector('#cardNumber')
+
+
+    cardNumber.addEventListener('keyup',()=>{
+        cFormPayment.querySelector('#iconCard').className = typeOfCard(cardNumber.value)
+    })
 
   
     //terminar pago
     cFormPayment.querySelector('#paymentForm').addEventListener("submit",(evt)=>{
         evt.preventDefault();
         
+        //remove all errors
         Array.from(cFormPayment.querySelectorAll('.c-form-inputError')).forEach(element=> element.remove())
 
-        const userName = cFormPayment.querySelector('#userName')
-        const userId = cFormPayment.querySelector('#userId')
-        const userEmail = cFormPayment.querySelector('#userEmail')
-        
-        const userState = cFormPayment.querySelector('#userState')
-        const userCity = cFormPayment.querySelector('#userCity')
-        const userAddress = cFormPayment.querySelector('#userAddress')
 
+        if(validateUserSection()){
 
-        //validate first section form
-        if(userName.value.length <= 5 && !isOnlyString(userName.value)){
-            userName.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Nombre incorrecto [a-zA-Z] {5-45} </div>')
+            if(validationCardSection() && document.getElementById('shopping-condition').checked){
+                document.body.appendChild(cModalInfo('Compra finalizada se ha enviado un correo con la informacion del ticket #123101','good'))
+            }
         }
-        if(!isNumber(userId.value) || userId.length < 5){
-            userId.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Numero de identificacion incorrecto [0-9] {5-45} </div>')
-        }
-        if(!isEmail(userEmail.value)){
-            userEmail.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Direccion de correcto incorrecta example@gmail.com </div>')
-        }
-
-        //validate second section form
-        if(userState.value.length <= 5 || !isOnlyString(userState.value)){
-            userState.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Nombre de Departamento incorrecto [a-zA-Z] {5-45}  </div>')
-        }
-        if(userCity.value.length <= 5 || !isOnlyString(userCity.value)){
-            userCity.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Nombre de Ciudad incorrecto [a-zA-Z] {5-45}  </div>')
-        }
-        if(userAddress.value.length <= 10){
-            userAddress.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Direccion de residencia incorrecta [a-zA-Z0-9] </div>')
-        }
-
-        
-
-        const cardUserName = cFormPayment.querySelector('#cardUserName')
-        const cardNumber = cFormPayment.querySelector('#cardNumber')
-        const validationNumberCard = cardValidation(cardNumber.value)
-        const cardExpireMonth = cFormPayment.querySelector('#cardExpireMonth')
-        const cardExpireYear = cFormPayment.querySelector('#cardExpireYear')
-        const cardVerificationCode = cFormPayment.querySelector('#cardVerificationCode')
-
-
-        //validation payment card
-        if(cardUserName.value.length <= 5 && !isOnlyString(cardUserName.value)){
-            cardUserName.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Nombre incorrecto [a-zA-Z] {5-45} </div>')
-            return
-        }
-        if(validationNumberCard.err){
-            document.body.appendChild(cModalInfo(validationNumberCard.err,'error'))
-            return
-        }
-        if(!isNumber(cardExpireMonth.value) || cardExpireMonth.value.length<2 || !isNumber(cardExpireYear.value) || cardExpireYear.value.length<2){
-            document.body.appendChild(cModalInfo('Fecha de expiracion incorrecta!','error'))
-            return
-        }
-        if(!isNumber(cardVerificationCode.value) || cardVerificationCode.value){
-            document.body.appendChild(cModalInfo('Codigo de verificacion Incorrecto!'),'error')
-            return
-        }
-
-
-
-
-
-    
     })
 
     cFormPayment.querySelector("#buttonHome").onclick = ()=>{ window.location.hash = ''}
@@ -225,7 +174,86 @@ const cFormPayment = () =>{
         document.querySelector('#totalPayment').textContent = seon.totalToPay() 
     }
 
+    function validateUserSection(){
+        const userName = cFormPayment.querySelector('#userName')
+        const userId = cFormPayment.querySelector('#userId')
+        const userEmail = cFormPayment.querySelector('#userEmail')
+        
+        const userState = cFormPayment.querySelector('#userState')
+        const userCity = cFormPayment.querySelector('#userCity')
+        const userAddress = cFormPayment.querySelector('#userAddress')
+        let validation = true
+
+        //validate first section form
+        if(userName.value.length <= 5 || !isOnlyString(userName.value)){
+            userName.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Nombre incorrecto [a-zA-Z] {5-45} </div>')
+            validation = false
+        }
+        if(!isNumber(userId.value) || userId.length < 5){
+            userId.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Numero de identificacion incorrecto [0-9] {5-45} </div>')
+            validation = false
+        }
+        if(!isEmail(userEmail.value)){
+            userEmail.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Direccion de correcto incorrecta example@gmail.com </div>')
+            validation = false
+        }
+
+        //validate second section form
+        if(userState.value.length <= 5 || !isOnlyString(userState.value)){
+            userState.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Nombre de Departamento incorrecto [a-zA-Z] {5-45}  </div>')
+            validation = false
+        }
+        if(userCity.value.length <= 5 || !isOnlyString(userCity.value)){
+            userCity.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Nombre de Ciudad incorrecto [a-zA-Z] {5-45}  </div>')
+            validation = false
+        }
+        if(userAddress.value.length <= 8){
+            userAddress.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Direccion de residencia incorrecta [a-zA-Z0-9], direccion especifica</div>')
+            validation = false
+        }
+
+
+        return validation
+        
+    }
+
+
+    function validationCardSection(){
+
+        const cardUserName = cFormPayment.querySelector('#cardUserName')
+        const cardNumber = cFormPayment.querySelector('#cardNumber')
+        const validationNumberCard = cardValidation(cardNumber.value)
+        const cardExpireMonth = cFormPayment.querySelector('#cardExpireMonth')
+        const cardExpireYear = cFormPayment.querySelector('#cardExpireYear')
+        const cardVerificationCode = cFormPayment.querySelector('#cardVerificationCode')
+
+        //validation payment card
+        if(cardUserName.value.length <= 5 || !isOnlyString(cardUserName.value)){
+            cardUserName.insertAdjacentHTML('afterend','<div class="c-form-inputError"> Nombre incorrecto [a-zA-Z] {5-45} </div>')
+            return false
+        }
+        if(validationNumberCard.err){
+            document.body.appendChild(cModalInfo(validationNumberCard.err,'error'))
+            return false
+        }
+
+        if(!isNumber(cardExpireMonth.value) || cardExpireMonth.value.length<2 || !isNumber(cardExpireYear.value) || cardExpireYear.value.length<2
+            || !validationDateExpire(cardExpireMonth.value,cardExpireYear.value)){
+            document.body.appendChild(cModalInfo('Fecha de expiracion incorrecta!','error'))
+            return false
+        }
+        if(!isNumber(cardVerificationCode.value) || cardVerificationCode.value.length > 3){
+            document.body.appendChild(cModalInfo('Codigo de verificacion Incorrecto!','error'))
+            return false
+        }
+
+        return true
+
+    }
+
+
     return cFormPayment
+
 }
 
 export {cFormPayment}
@@ -234,56 +262,54 @@ export {cFormPayment}
 //input string -> '5123951989745802' return {valor:70: validation:true}
 function cardValidation(numeros) {
     
-    let numberCard = numeros
-    var suma = 0;
+    let sum = 0
 
-    if(numberCard == undefined || numberCard.length !== 16 || isNaN(numeros)){
-        return {valor:suma, validation:false, err:'Numero de tarjeta incorrecto'}
+    if(numeros == undefined || numeros.length < 15 || numeros.length > 16 || isNaN(numeros)){
+        return {valor:sum, validation:false, err:'Numero de tarjeta incorrecto'}
+    }
+    
+    let lista =  numeros.split('').reverse().map((x)=> parseInt(x))
+
+    lista.forEach((number,index)=>{
+
+        //count index + 1 because the index is [0,1,2,3,4] but we need to evaluate [1,2,3,4,5]
+        if((index+1) % 2 == 0){
+            sum += Math.floor(number*2 / 10) + number * 2 % 10
+        }else{
+            sum += number
+        }
+
+    })
+
+
+    if(sum % 10 == 0){
+        return {valor:sum, validation:true, err:null}
+    }
+    else{
+        return {valor:sum, validation:false, err:'Numero de tarjeta incorrecto'}
     }
 
-   for(let i = 0; i<numberCard.length; i += 2){
-
-       let number = numberCard[i]*2;
-
-       number >= 10 
-       ? suma += ((number%10) + 1)
-       : suma+= number
-
-       i+1 < numberCard.length
-       ? suma+= parseInt(numberCard[i+1])
-       : suma;
-
-   }
-
-   //los retornos no se pueden colocar en condicionales ternarias a menos que aplique cosas raras
-   if(suma%10 == 0){
-       return {valor:suma, validation:true, err:null}
-   }else{
-       return {valor:suma, validation:false, err:'Numero de tarjeta incorrecto'}
-   }
 }
 
 
 
-function isNumber(string,min=1,max=45){
-    // let reg = new RegExp(`^[0-9]{}$`);
-    let reg = new RegExp(`^[0-9]+$`);
+//02-22 - today >= 00-10 years = true
+function validationDateExpire(month,year){
 
-    return reg.test(string)
-}
+    const actuallyYear = new Date().getFullYear() % 2000
+    const actuallyMonth = new Date().getMonth() + 1
+    
+    const cardMonth = parseInt(month,10)
 
+    const cardYear = parseInt(year,10)
 
-function isEmail(string){
+    if(cardYear - actuallyYear == 0 && cardMonth >= actuallyMonth){
+        return true
+    }
+    else if(cardYear - actuallyYear >= 0
+        && cardYear - actuallyYear <= 10
+        && cardMonth >= 0 && cardMonth <= 12){
 
-    // if(!string.includes('@') || string.indexOf('@') < 5 || string.includes(' ') || (string.length - string.indexOf('@') < 8) ){
-    //     return false
-    // }
-
-    // return true
-
-    var mailformat = /\S+@\S+\.\S+/;
-
-    if(string.match(mailformat)){
         return true
     }
     else{
@@ -292,14 +318,54 @@ function isEmail(string){
 
 }
 
+//visa, mastercard, discover  american express needs 15 digits we need to change the validation
+function typeOfCard(numero){
+    let master = /5[1-5][0-9]{14}$/
+    let visa = /^4[0-9]{12}(?:[0-9]{3})?$/
+    let discover = /^6(?:011|5[0-9]{2})[0-9]{12}$/
+    let americanExpress = /^3[47][0-9]{13}$/
+
+    if(master.test(numero)){
+        return 'fab fa-cc-mastercard'
+    }
+    else if(visa.test(numero)){
+        return 'fab fa-cc-visa'
+    }
+    else if(discover.test(numero)){
+        return 'fab fa-cc-discover'
+    }
+    else if(americanExpress.test(numero)){
+        return 'fab fa-cc-amex'
+    }
+    else{
+        return 'fas fa-credit-card'
+    }
+    
+}
+
+
+//type basics Number, Email, onlystring
+function isNumber(string,min=1,max=45){
+    // let reg = new RegExp(`^[0-9]{}$`);
+    let reg = new RegExp(`^[0-9]+$`);
+
+    return reg.test(string)
+}
+
+function isEmail(string){
+    
+    var mailformat = /\S+@\S+\.\S+/;
+
+    return mailformat.test(string)
+
+}
+
 function isOnlyString(string){
-    let reg = /^[A-Za-z]+$/;  //parece ser que un string con / aca iria / es un regex
+    let reg = /^[A-Za-z_ ]+$/;
     
     return reg.test(string)
 }
 
-
-isOnlyString('')
 // let cardNumber = ''
 
   //if i want to simulate password without last four numbers
@@ -326,10 +392,3 @@ isOnlyString('')
     //     }
     
     // })
-
-
-
-
-
-    var numbers = '[0-9]'
-    console.log('asd123'.match(numbers))
